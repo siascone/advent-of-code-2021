38b811e0c3a8846854f18bb08685e4e0f0ed74ac
siascone <spencer.iascone@gmail.com>
2021-12-03 19:41:15 -0700

    Git init and commit first puzzle

---
+++ day-1/puzzle-1.rb
@@ -0,0 +1,21 @@
+
+
+
+def num_increasing_depths(depths)
+
+    count = 0
+    previous = depths.first
+
+    (1...depths.length).each do |idx|
+        current_depth = depths[idx]
+        if current_depth > previous
+            count += 1
+        end
+        previous = current_depth
+    end
+
+    return count
+end
+    
+depths_arr = File.readlines('puzzle', chomp: true).map{ |ele| ele.to_i }
+p num_increasing_depths(depths_arr)
